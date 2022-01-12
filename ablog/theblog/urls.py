43f867af9186dc7e15a916fde8f16f5f1a00d28a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-12 10:45:41 +0600

    Delete a Blog Post

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import detail
 
 # from . import views
-from .views import AddPostView, HomeView, ArticleDateDetailView, UpdatePostView
+from .views import AddPostView, HomeView, ArticleDateDetailView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     # path("", views.home, name="home"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path("article/<int:pk>", ArticleDateDetailView.as_view(), name="article-detail"),
     path("add_post/", AddPostView.as_view(), name="add_post"),
     path("article/edit/<int:pk>", UpdatePostView.as_view(), name="update_post"),
+    path("article/<int:pk>/remove", DeletePostView.as_view(), name="delete_post"),
 ]
