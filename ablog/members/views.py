26896ea0244d75f4a4d4fa536f6fbe9948e12fd6
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-12 12:18:33 +0600

    Creating Blog User Logins With Authentication

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+# registration.html
