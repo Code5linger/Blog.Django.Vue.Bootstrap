08d6cc1f9ad6102b266dc1cc6902faf3cc69ae93
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-16 19:46:49 +0600

    Add Blog Category Navbar Links

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import detail
 
 # from . import views
-from .views import AddPostView, HomeView, ArticleDateDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import AddPostView, HomeView, ArticleDateDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     # path("", views.home, name="home"),
@@ -13,4 +13,5 @@ urlpatterns = [
     path("article/edit/<int:pk>", UpdatePostView.as_view(), name="update_post"),
     path("article/<int:pk>/remove", DeletePostView.as_view(), name="delete_post"),
     path("category/<str:cats>/", CategoryView, name="category"),
+    path("category-list/", CategoryListView, name="category-list"),
 ]
