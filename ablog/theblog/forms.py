d3ba1b9645f6f24d3376d9bd2be7d170482755dd
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-17 15:33:55 +0600

    Add Article Snippets

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -12,7 +12,8 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("title", "title_tag", "author", "category", "body")
+        fields = ("title", "title_tag", "author",
+                  "category", "body", 'snippet')
 
         widgets = {
             "title": forms.TextInput(
@@ -23,6 +24,7 @@ class PostForm(forms.ModelForm):
             # "author": forms.Select(attrs={"class": "form-control"}),
             "category": forms.Select(choices=choice_list, attrs={"class": "form-control"}),
             "body": forms.Textarea(attrs={"class": "form-control"}),
+            "snippet": forms.Textarea(attrs={"class": "form-control"}),
         }
 
 
