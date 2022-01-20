774b14da930801c4a2eba04ddcbe8886db1ce08b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-20 19:17:31 +0600

    Style Our Profile Page With Bootstrap

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 
-from .forms import SignUpForm
+from .forms import EditProfileForm, SignUpForm
 
 # Create your views here.
 
@@ -15,11 +15,9 @@ class UserRegisterView(generic.CreateView):
 
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 
     def get_object(self):
         return self.request.user
-
-
