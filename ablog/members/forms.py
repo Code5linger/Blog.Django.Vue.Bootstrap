e4ecf1e973648206ffee73ddf824a7145d6d29e7
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-21 20:40:36 +0600

    Style Our Profile Page With Bootstrap

--- ablog/members/forms.py
+++ ablog/members/forms.py
@@ -37,11 +37,11 @@ class EditProfileForm(UserChangeForm):
     last_login = forms.CharField(max_length=100, widget=forms.TextInput(
         attrs={"class": "form-control"}))
     is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(
-        attrs={"class": "form-control"}))
+        attrs={"class": "form-check"}))
     is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(
-        attrs={"class": "form-control"}))
+        attrs={"class": "form-check"}))
     is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(
-        attrs={"class": "form-control"}))
+        attrs={"class": "form-check"}))
     date_joined = forms.CharField(max_length=100, widget=forms.TextInput(
         attrs={"class": "form-control"}))
 
