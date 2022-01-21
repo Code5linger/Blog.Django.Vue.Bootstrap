327b746c99df42f1d3930424930949754b346a1e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-21 21:19:10 +0600

    Suspended For Now!

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 
 from .forms import EditProfileForm, SignUpForm
 
 # Create your views here.
+
+
+class PasswordsChangeView(PasswordChangeView):
+    from_class = PasswordChangeForm
+    success_url = reverse_lazy('password_success.html')
+    #success_url = reverse_lazy('home')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
 
 
 class UserRegisterView(generic.CreateView):
