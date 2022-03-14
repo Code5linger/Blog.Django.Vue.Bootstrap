2613f6bf2c414626ab344d0543ab2ee540b0f499
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-15 04:28:45 +0600

    Navigation

--- new-gatsby-project/src/pages/index.js
+++ new-gatsby-project/src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world👋🏼!</div>
-}
+export default () => (
+  <div>
+    <div>
+      <h1>First Gatsby project</h1>
+      <Link to="http://localhost:8000/test">Navigate to Test</Link>
+      <br />
+      <a href="https://www.google.com/">Google</a>
+      <br />
+      <button onClick={() => console.log("Home")}>Home</button>
+    </div>
+
+    <img
+      src="https://c.tenor.com/K3FR6N_5QjAAAAAM/drama-2013.gif"
+      width="25%"
+      alt=""
+    />
+  </div>
+)
--- new-gatsby-project/src/pages/test.js
+++ new-gatsby-project/src/pages/test.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { navigate } from "gatsby"
 
 const Test = () => (
   <div>
     <h1>Testing!</h1>
     <h1>Still Testing!</h1>
+    <button onClick={() => navigate("/")}>Home</button>
   </div>
 )
 
